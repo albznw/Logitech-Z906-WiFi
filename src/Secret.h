00fa311d79a1ae25919ea5bc3f27a6be12e04df0
Albin Winkelmann <albin-w@hotmail.com>
2018-09-12 22:30:12 +0200

    Secret.h file skeleton

---
+++ src/Secret.h
@@ -0,0 +1,10 @@
+const char* ssid = "..........";
+const char* password = "..........";
+
+/****************************** MQTT - Settings *******************************/
+#define Broker          ""
+#define Port            1883
+
+#define MQTTClientId        ""
+#define MQTTUsername        ""
+#define MQTTPassword        ""
